d73cba9b8ae5c0a92b40de517e2c249c8300b143
ramtin_ehsnai <ramtin.ehsani79@gmail.com>
2021-06-14 18:47:26 +0430

    Fix: some issues

--- Phase 2/Compress.m
+++ Phase 2/Compress.m
@@ -1,28 +1,32 @@
+clc
+clear
+close all
+
 %% Read File
 [x,fs]=audioread('in.wav');
-length=length(x);
+len=length(x);
 player=audioplayer(x,fs);
 player.play
 
 %% Orginal
-t=fft(x,length);
+t=fft(x,len);
 X=fftshift(t);
-f=-fs/2:fs/length:(fs/2-fs/length);
+f=-fs/2:fs/len:(fs/2-fs/len);
 
 figure(1)
 plot(f,abs(X))
 title('Original Audio Signal')
-xlabel('Freq(hq)');
+xlabel('Freq');
 ylabel('Magnitude');
 
 %% Compress
-Xr=zeros(1,length);
-Xr((length/4)+1:(3*length/4))= X((length/4)+1:(3*length/4));
+Xr=zeros(1,len);
+Xr((len/4)+1:(3*len/4))= X((len/4)+1:(3*len/4));
 
 figure(2)
 plot(f, abs((Xr)));
 xr= real(ifft(fftshift(Xr)));
 audiowrite('Compressed.wav',xr,fs);
-title('Compressed Audio')
-xlabel('Freq(hq)');
+title('Compressed Audio Signal')
+xlabel('Freq');
 ylabel('Magnitude');
